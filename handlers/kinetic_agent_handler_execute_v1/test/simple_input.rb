b2e1de70f91feca231ee6ba18f85c4725e66d852
Chad Rehm <chad.rehm@kineticdata.com>
2020-02-05 10:32:39 -0600

    PER-182 added agent handler
    Originally the project was in another repo.  This is the first addition of the handler to platform repo.
    Ticket 182 refers to changes made so that the handler would manage errors more constantly.

---
+++ handlers/kinetic_agent_handler_execute_v1/test/simple_input.rb
@@ -0,0 +1,23 @@
+{
+  'info' => {
+    'api_server' => '',
+    'api_username' => '',
+    'api_password' => '',
+    'enable_debug_logging'=>'yes'
+  },
+  'parameters' => {
+    'error_handling' => 'Error Message',
+    'handler_space' => 'Test',
+    'handler_slug' => 'submission_retrieve',
+    'payload' => 
+      '{
+        "error_handling": "Raise Error",
+        "space_slug": "",
+        "retrieve_by": "Id",
+        "kapp_slug": "services",
+        "form_slug": "general-facilities-request",
+        "query": "",
+        "submission_id": "f59ee2cc-3932-11ea-9059-c30fd4c36c6a"
+      }',
+  }
+}
