f3ae3a7e8a15aac6076f93632a5b93dfd7fefc14
briapete <brian.peterson@kineticdata.com>
2021-12-16 12:50:48 -0600

    kinetic_request_ce_attachment_create_v2 initial version modified from v1 to use authentication to get attachements

---
+++ handlers/kinetic_request_ce_attachment_copy_v2/test/simple_input.rb
@@ -0,0 +1,18 @@
+{
+  'info' => {
+    'api_server' => 'https://<YOUR_SPACE>.kinops.io',
+    'api_username' => '',
+    'api_password' => '',
+    'space_slug' => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'space_slug' => '',
+    'field_name' => '',
+    'submission_id' => '',
+    'to_field_name' => '',
+    'to_submission_id' => '',
+    'kapp_slug' => '',
+    'form_slug' => '',
+  }
+}
