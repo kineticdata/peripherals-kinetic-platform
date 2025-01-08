814ea0a020ccf38932026a9d4c01817881d0e2ea
Travis Wiese <travis.wiese246@gmail.com>
2025-01-08 16:09:58 -0600

    hotfix tested and confirmed working on sqlserver and postgresql

--- handlers/kinetic_request_ce_submission_db_insert_v1/handler/init.rb
+++ handlers/kinetic_request_ce_submission_db_insert_v1/handler/init.rb
@@ -150,7 +150,7 @@ class KineticRequestCeSubmissionDbInsertV1
       @db.identifier_output_method = nil
       @max_db_identifier_size = 30
       @using_oracle = true
-    else
+    else #postgresql
       jdbc_url_opts.concat("&") if jdbc_url_opts.empty? == false && jdbc_url_opts[-1] != "&"
       @max_db_identifier_size = 64 if @info_values["jdbc_database_id"].downcase == "postgresql"
       # TODO: Fix JDBC URL connection string to not take unsanitized user info values and separate username/password from connection string.
@@ -216,6 +216,23 @@ class KineticRequestCeSubmissionDbInsertV1
     form_slug = nil
     kapp_table_name = nil
     form_table_name = nil
+
+
+    #HOTFIX - Check/update column_definitions if previous value (8) for fieldKey 
+
+    #If SQLServer or postgresql
+    begin
+      fieldKeySize = check_field_size('column_definitions', 'fieldKey')
+      if fieldKeySize.to_i == 8
+        alter_column_type_size('column_definitions', 'fieldKey', 'varchar', @db_column_size_limits[:fieldKey])
+      end
+    rescue
+
+    end
+   
+
+    #If Oracle - Above may work, untested
+
 
     # Get kapp & form slug / datastore info.
     if submission.nil? == false then
@@ -354,11 +371,6 @@ class KineticRequestCeSubmissionDbInsertV1
               {k.to_sym => v}
             }.reduce Hash.new, :merge
 
-          #HOTFIX - Update fieldKey size
-          fieldKeySize = check_field_size(form_table_name, 'fieldKey')
-          if fieldKeySize == 8
-            alter_column_type_size(form_table_name, 'fieldKey', 'varchar', @db_column_size_limits[:fieldKey])
-          end
 
           @db[form_table_name.to_sym].call(
             :insert,
@@ -440,7 +452,7 @@ class KineticRequestCeSubmissionDbInsertV1
             :api_password => api_password
           })
         end
-
+        
         # Add columns if the kapp table does not have all kapp fields defined
         update_kapp_table_columns({
           :kapp_slug => kapp_slug
@@ -1308,15 +1320,35 @@ end
 #
 ##########################################################################################################
 
+#Need to account for db_type
 def check_field_size(tableName, fieldName)
   size = nil
   @db.schema(tableName.to_sym).each {|label,columnDetails| 
     if label.to_s == fieldName
       #Return size
-      size = columnDetails[:max_chars]
+      if @info_values["jdbc_database_id"].downcase == "postgresql"
+        puts "Found Postgres" if @enable_debug_logging
+        if columnDetails[:db_type].match?( /\(\d+\)/)
+          size = columnDetails[:db_type][/\(.*?\)/].delete('()')
+        else
+          puts "Non-numeric: #{columnDetails[:db_type]}" if @enable_debug_logging
+          #How to handle non-numerical fields
+        end
+        
+      elsif @info_values["jdbc_database_id"].downcase == 'sqlserver'
+        puts "Found sqlserver" if @enable_debug_logging
+        size = columnDetails[:max_chars]
+      elsif @info_values["jdbc_database_id"].downcase == 'oracle'
+        #TODO
+      else
+        puts "Else catch" if @enable_debug_logging
+        #TODO - catch case
+      end
+      puts "Size found: #{size}" if @enable_debug_logging
       return size
     end
   }
+  puts "No size found: #{size}" if @enable_debug_logging
   return size
 end
 
@@ -1329,7 +1361,20 @@ end
 ##########################################################################################################
 
 def alter_column_type_size(tableName, fieldName, dataType, fieldSize)
-  @db.alter_table(tableName.to_sym) do
-    set_column_type(fieldName.to_sym, dataType.to_sym, size: fieldSize)
+  case (@info_values["jdbc_database_id"])
+  when 'postgresql'
+    puts "Altering #{fieldName} column in #{@info_values["jdbc_database_id"]} - new type/size #{dataType}-#{fieldSize}" if @enable_debug_logging
+    @db.alter_table(tableName.to_sym) do
+      set_column_type(:fieldKey, "#{dataType}(#{fieldSize})")
+    end
+  when 'sqlserver'
+    puts "Altering #{fieldName} column in #{@info_values["jdbc_database_id"]} - new type/size #{dataType}-#{fieldSize}" if @enable_debug_logging
+    @db.alter_table(tableName.to_sym) do
+      set_column_type(fieldName.to_sym, dataType.to_sym, size: fieldSize)
+    end
+  when 'oracle'
+
+  else
+  
   end
 end
